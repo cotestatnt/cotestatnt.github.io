d3fa9b696617f931da5f3031702996985e8569e9
Tolentino Cotesta <cotestatnt@yahoo.com>
2021-09-07 07:21:29 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -17,10 +17,8 @@ var config = {
   authDomain: "esp-stream-test.firebaseapp.com",
   databaseURL: "https://esp-stream-test-default-rtdb.europe-west1.firebasedatabase.app",
   projectId: "esp-stream-test",
-  storageBucket: "esp-stream-test.appspot.com",
-  messagingSenderId: "238762683803",
+  storageBucket: "esp-stream-test.appspot.com",  
   appId: "1:238762683803:web:2b310f1bc33d861cb2053e",
-  measurementId: "G-8JWHBP5X3B"
 };
 firebase.initializeApp(config);
 var database = firebase.database();
