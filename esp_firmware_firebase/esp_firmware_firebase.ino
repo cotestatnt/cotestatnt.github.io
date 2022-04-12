c6e7df6350c8020a8bdc353f7e6bc560d70e006a
Tolentino Cotesta <cotestatnt@yahoo.com>
2022-04-12 09:47:40 +0200

    Update esp_firmware_firebase.ino
    - ArduinoJson
    - range based for()

--- esp_firmware_firebase/esp_firmware_firebase.ino
+++ esp_firmware_firebase/esp_firmware_firebase.ino
@@ -1,31 +1,4 @@
-#if defined(ESP32)
-#include <WiFi.h>
-#elif defined(ESP8266)
-#include <ESP8266WiFi.h>
 
-#endif
-#include <Firebase_ESP_Client.h>
-#include "addons/TokenHelper.h"
-#include "addons/RTDBHelper.h"
-
-
-struct SetPin {
-  bool en = false;
-  int pin = -1;
-  int level = -1;
-};
-
-// List of inputs
-const uint8_t inputs[] = { D5, D6, D7 };
-const char inLabels[][10] = {"D5-IN", "D6-IN", "D7-IN"};
-
-// List of outputs
-const uint8_t outputs[] = { D3, LED_BUILTIN };
-const char outLabels[][10] = {"D3-OUT", "D4-OUT"};
-
-// Usually LED_BUILTIN turn on with level LOW on pin D4, 
-// but can be useful also for relay board
-const bool outInvert[] = {false, true};
 
 /* 1. Define the WiFi credentials */
 #define WIFI_SSID "xxxxxxxxxxxxxx"
@@ -39,128 +12,165 @@ const bool outInvert[] = {false, true};
 #define USER_EMAIL "xxxxxxxxxxxx.xxxxxxxxxxx@email.com"
 #define USER_PASSWORD "xxxxxxxxxxxxxxxxxxx"
 
-FirebaseData fbdo1;     // Firebase data object
+#ifdef ESP8266
+  #include <ESP8266WiFi.h>
+  #define INP_A 14  // D5
+  #define INP_B 12  // D6
+  #define INP_C 13  // D7
+  #define OUT_A 2   // D2
+  #define OUT_B 0   // D3
+  #define OUT_C 4   // D4  
+#elif defined(ESP32)
+  #include <WiFi.h>
+  #define INP_A 14  // D13
+  #define INP_B 12  // D12
+  #define INP_C 13  // D13
+  #define OUT_A 15  // D15
+  #define OUT_B 2   // D2
+  #define OUT_C 4   // D4
+#endif
+
+// Even if Firebase_ESP_Client has it's own JSON parser, I prefer this library for JSON parsing
+#include <ArduinoJson.h>
+
+#include <Firebase_ESP_Client.h>
+#include "addons/TokenHelper.h"
+#include "addons/RTDBHelper.h"
+
+// a struct for storing GPIO properties
+struct PinGPIO {
+  uint8_t pin;
+  const char* label;
+  bool invert;
+};
+
+// Define a list of GPIO inputs
+PinGPIO Inputs[] = {
+  {INP_A, "Input A", true},   /* Pin number, label, pullup */
+  {INP_B, "Input B", true},
+  {INP_C, "Input C", true},
+};
+
+// Define a list of GPIO outputs
+PinGPIO Outputs[] = {
+  {OUT_A, "Output A", true},  /* Pin number, label, active low */
+  {OUT_B, "Output B", false},
+  {OUT_C, "Output C", false},
+};
+
+
+FirebaseData stream;    // Firebase data object
+FirebaseData fbdo;      // Firebase data object
 FirebaseAuth auth;      // Firebase authentication object
 FirebaseConfig config;  // Firebase configuration object
 const char* path = "/esp-stream";
 
+// Prepare the JSON payload with updated state of GPIO list and send to Firebase RTDB
 void updateGpioList() {
-    String jsonStr;
-    FirebaseJson gpios;
-    size_t pos = 0;
-    for (uint8_t i=0; i < sizeof(inputs); i++) {
-        FirebaseJson gpio;
-        gpio.add("pin", inputs[i]);
-        gpio.add("label", String(inLabels[i]));
-        gpio.add("type", "input");
-        gpio.add("level", digitalRead(inputs[i]) ? true : false);
-        String key = "gpios/[" + String(pos++) + "]";
-        gpios.set(key, gpio);
+  String jsonStr;
+  
+  // Add scope in order to destroy properly DynamicJsonDocument object
+  { 
+    DynamicJsonDocument doc(1024);
+    JsonArray gpios = doc.createNestedArray("gpios");
+    
+    for (PinGPIO& input : Inputs ) {    
+      JsonObject gpio = gpios.createNestedObject();
+      gpio["type"] = "input";
+      gpio["pin"] = input.pin;
+      gpio["label"] = input.label;    
+      gpio["level"] = digitalRead(input.pin);   
     }
-    for (uint8_t i=0; i < sizeof(outputs); i++) {
-        FirebaseJson gpio;
-        gpio.add("pin", outputs[i]);
-        gpio.add("label", String(outLabels[i]));
-        gpio.add("type", "output");
-        gpio.add("invert", outInvert[i]);
-        gpio.add("level", digitalRead(outputs[i]) ? true : false);
-        String key = "gpios/[" + String(pos++) + "]";
-        gpios.set(key, gpio);
+    
+    for (PinGPIO& output : Outputs ) {
+      JsonObject gpio = gpios.createNestedObject();
+      gpio["type"] = "output";
+      gpio["pin"] = output.pin;
+      gpio["label"] = output.label;    
+      gpio["level"] = digitalRead(output.pin);   
+      gpio["invert"] = output.invert;        
+    }  
+    //serializeJsonPretty(doc, Serial);    
+    serializeJson(doc, jsonStr); 
+  } 
+
+  if (Firebase.ready()) {    
+    FirebaseJson fb_json;
+    fb_json.setJsonData(jsonStr);
+    if (!Firebase.RTDB.setJSONAsync(&fbdo, path, &fb_json)) {
+      Serial.println("FAILED");
+      Serial.println("REASON: " + fbdo.errorReason());
+      Serial.println();
     }
-    if(Firebase.ready()) {
-      if (!Firebase.RTDB.setJSONAsync(&fbdo1, path, &gpios)) {
-        Serial.println("FAILED");
-        Serial.println("REASON: " + fbdo1.errorReason());      
-        Serial.println();
-      }
-    }
+  }
+  
 }
 
-
-bool updateGpioState(){
-  // With this variable, we will check if some of defined gpios has changed
+// Return true if some GPIO has changed the state
+bool updateGpioState() {
+  // With this word, we will check if some of defined gpios has changed
   uint16_t gpioState = 0;
   static uint16_t gpioLast;
   size_t pos = 0;
-  for (uint8_t i=0; i < sizeof(inputs); i++) {
-      gpioState = (digitalRead(inputs[i]) << pos++) | gpioState;
-  }
-  for (uint8_t i=0; i < sizeof(outputs); i++) {
-      gpioState = (digitalRead(outputs[i]) << pos++) | gpioState;
-  }
-  if(gpioState != gpioLast) {
+  // Check outputs
+  for (PinGPIO& output : Outputs )
+    gpioState = (digitalRead(output.pin) << pos++) | gpioState;
+  // Check inputs
+  for (PinGPIO& input : Inputs )
+    gpioState = (digitalRead(input.pin) << pos++) | gpioState;
+
+  if (gpioState != gpioLast) {
     gpioLast = gpioState;
     return true;
   }
-  return false; 
+  return false;
 }
 
 
 // This is the callback function called when stream from firebase was received
 void streamCallback(FirebaseStream data) {
-    Serial.println("\nStream Data1 available...");
-    Serial.println("STREAM PATH: " + data.streamPath());
-    Serial.println("EVENT PATH: " + data.dataPath());
-    Serial.println("DATA TYPE: " + data.dataType());
-    Serial.println("EVENT TYPE: " + data.eventType());
-    
-    if (data.dataType() == "json") {
-        FirebaseJson &json = data.jsonObject();
-        
-        String key, value = "";
-        int type = 0;
-        SetPin setPin;
-      
-        // Iterate the JSON data in order to parse all keys and values
-        size_t len = json.iteratorBegin();
-        for (size_t i = 0; i < len; i++) {
-            // Get all key:value combination for this JSON event
-            json.iteratorGet(i, type, key, value);  
+  Serial.println("\nStream Data available...");
+  Serial.println("STREAM PATH: " + data.streamPath());
+  Serial.println("EVENT PATH: " + data.dataPath());
+  Serial.println("DATA TYPE: " + data.dataType());
+  Serial.println("EVENT TYPE: " + data.eventType());
 
-            // With string values, we have unwanted " chars inserted from library while parsing
-            value.replace('\"', ' ');
-            value.trim();
-            Serial.printf("\"%s\":\"%s\"\n", key.c_str(), value.c_str());              
-
-            // If this is a "writeOut" command, set the pin structure ready to be valorized
-            if(key.equals("cmd") && value.equals("writeOut")) {
-                setPin.en = true;
-                Serial.println("cmd: writeOut");
-            }
-            
-            // If true, we are parsing a "writeOut" command
-            if(setPin.en) {               
-              if(key.equals("pin"))   setPin.pin = value.toInt();
-              if(key.equals("level")) setPin.level = value.equals("true") ? 1 : 0;
-
-              // All variables valorized with parsed values, set output and clear pin structure
-              if(setPin.level > -1 && setPin.pin > -1) {
-                Serial.printf("Set output pin %d: %d\n", setPin.pin, setPin.level);
-                digitalWrite(setPin.pin, setPin.level);
-              }
-            }
-        }
-        json.iteratorEnd();
+  if (data.dataType() == "json") {
+    DynamicJsonDocument doc(1024);
+    DeserializationError error = deserializeJson(doc, data.jsonString());    
+    if (error) {
+      Serial.print(F("deserializeJson() failed: "));
+      Serial.println(error.f_str());
+      return;
     }
+    
+    //serializeJsonPretty(doc, Serial);    
+    String command = doc["cmds"]["cmd"].as<String>();
+    if (command.equals("writeOut")) {      
+      uint8_t pin = doc["cmds"]["pin"];
+      bool level = doc["cmds"]["level"];
+      Serial.printf("\n%s %d %d\n", command.c_str(), pin, level);            
+      digitalWrite(pin, level);
+    }
+  }
 }
 
-void streamTimeoutCallback(bool timeout)
-{
+void streamTimeoutCallback(bool timeout) {
   if (timeout)
-  {
-    Serial.println();
-    Serial.println("Stream timeout, resume streaming...");
-    Serial.println();
-  }
+    Serial.println("stream timed out, resuming...\n");
+
+  if (!stream.httpConnected())
+    Serial.printf("error code: %d, reason: %s\n\n", stream.httpCode(), stream.errorReason().c_str());
 }
 
 void setup() {
   // Set pinMode for input and output pins
-  for (uint8_t i = 0; i < sizeof(inputs); i++)
-    pinMode(inputs[i], INPUT_PULLUP);
-  for (uint8_t i = 0; i < sizeof(outputs); i++)
-    pinMode(outputs[i], OUTPUT);
-    
+  for (PinGPIO& input : Inputs )
+    pinMode(input.pin, input.invert ? INPUT_PULLUP : INPUT);
+
+  for (PinGPIO& output : Outputs )
+    pinMode(output.pin, OUTPUT);
+
   Serial.begin(115200);
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
   Serial.print("Connecting to Wi-Fi");
@@ -189,29 +199,24 @@ void setup() {
 
 #if defined(ESP8266)
   //Set the size of WiFi rx/tx buffers in the case where we want to work with large data.
-  fbdo1.setBSSLBufferSize(1024, 1024);
+  stream.setBSSLBufferSize(2048, 2048);
+  stream.setResponseSize(1024);
 #endif
 
-  //Set the size of HTTP response buffers in the case where we want to work with large data.
-  fbdo1.setResponseSize(1024);
-
-  //The data under the node being stream (parent path) should keep small
-  //Large stream payload leads to the parsing error due to memory allocation.
-  if (!Firebase.RTDB.beginStream(&fbdo1, path)) {
+  if (!Firebase.RTDB.beginStream(&stream, path)) {
     Serial.println("\nCan't begin stream connection...");
-    Serial.println("REASON: " + fbdo1.errorReason());
+    Serial.println("REASON: " + stream.errorReason());
     Serial.println("------------------------------------\n");
   }
 
-  // Set callback for stream data
-  Firebase.RTDB.setStreamCallback(&fbdo1, streamCallback, streamTimeoutCallback);
+  // Set callback functions for stream data
+  Firebase.RTDB.setStreamCallback(&stream, streamCallback, streamTimeoutCallback);
   updateGpioList();
 }
 
 void loop() {
-
   // True on pin state change
-  if( updateGpioState()) {
+  if ( updateGpioState()) {
     updateGpioList();
   }
 }
